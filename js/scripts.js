1d2c059e6951cf16ae01a36307d0bc75534c1ea8
aferrar <aferrar90@gmail.com>
2024-03-06 22:35:42 -0500

    Updated splash page intro visibility on load and wordArray

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@
     // Remove no-js class
     $('html').removeClass('no-js');
 
+    // Hide #lead-content for the first 12 seconds and then fade in
+    $('#lead-content').hide();
+    setTimeout(function() {
+        $('#lead-content').fadeIn(3000); // 1000ms for a smooth fade in effect
+    }, 13000);
+
     // Animate to section when nav is clicked
     $('header a').click(function(e) {
 
@@ -84,7 +90,7 @@
 
     // Alternate descriptor text every 5 seconds
     count = 1;
-    wordArray = [" Analysis", "Asset Management", "Automation", " Design", " Web Development"];
+    wordArray = [" Analytics", "Asset Management", "Applications", "Big Data ETLs", "Consulting", "Dashboards", "Design"];
     function word_swap() {
         $("#word").fadeTo(750, 0, function() {
             $(this).text(wordArray[count % wordArray.length]).fadeTo(750, 1);
